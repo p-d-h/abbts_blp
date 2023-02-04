e7d3a1e0d908fe52e5deb631c125c33d8f6627db
Pascal Helfenstein <pascal.helfenstein@xemax.ch>
2023-02-04 15:24:10 +0100

    Integration sphinx

--- build_docs.bat
+++ build_docs.bat
@@ -1,7 +1,7 @@
 REM HTML Renderer
 REM https://raw.githack.com/p-d-h/abbts_blp/main/docs/build/html/index.html
 
-rmdir docs\build /S /Q
+REM rmdir docs\build /S /Q
 
 venv\Scripts\python.exe docs\source\demo\python_jinja.py
 
