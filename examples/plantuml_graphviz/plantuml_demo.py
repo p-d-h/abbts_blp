89aed8bbc42ed14a842efd65bca36b46a8f3d474
Pascal Helfenstein <pascal.helfenstein@xemax.ch>
2023-04-02 14:04:42 +0200

    plantuml

--- examples/plantuml_graphviz/plantuml_demo.py
+++ examples/plantuml_graphviz/plantuml_demo.py
@@ -1,5 +1,13 @@
 # Plantuml
 # https://plantuml.com/de/state-diagram
+# https://real-world-plantuml.com/
+# https://dev.to/sakko/how-i-code-not-draw-my-er-diagram-4kc3
+# https://nevets.github.io/2019/06/03/plantuml/
+# https://plantuml.com/de-dark/ie-diagram
+# https://plantuml.com/de/skinparam
+# https://plantuml-documentation.readthedocs.io/en/latest/formatting/all-skin-params.html
+# https://plantuml.com/es/class-diagram
+
 
 import os, sys
 import pathlib
@@ -15,6 +23,7 @@ program_with_args = [
     'java', '-jar',
     f'{path_tools}/plantuml-1.2023.4.jar',
     f'{path}/plantuml_demo_state_diagram.txt',
+    '-charset', 'UTF-8',
     '-png']
 
 CREATE_NO_WINDOW = 0x08000000
